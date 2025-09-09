f4454a762e82285b65eb36c8791ff97d8797d381
cheildo <cheickzida@gmail.com>
2025-09-10 04:16:47 +0800

    Create orchestration proto file

--- api/proto/nexusclash/v1/game_orchestration.pb.go
+++ api/proto/nexusclash/v1/game_orchestration.pb.go
@@ -9,7 +9,9 @@ package nexusclashv1
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 	reflect "reflect"
+	sync "sync"
 )
 
 const (
@@ -19,13 +21,81 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
+type StatusResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Status         string `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty"`
+	RunningServers int64  `protobuf:"varint,2,opt,name=running_servers,json=runningServers,proto3" json:"running_servers,omitempty"`
+}
+
+func (x *StatusResponse) Reset() {
+	*x = StatusResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_nexusclash_v1_game_orchestration_proto_msgTypes[0]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *StatusResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*StatusResponse) ProtoMessage() {}
+
+func (x *StatusResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_nexusclash_v1_game_orchestration_proto_msgTypes[0]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use StatusResponse.ProtoReflect.Descriptor instead.
+func (*StatusResponse) Descriptor() ([]byte, []int) {
+	return file_nexusclash_v1_game_orchestration_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *StatusResponse) GetStatus() string {
+	if x != nil {
+		return x.Status
+	}
+	return ""
+}
+
+func (x *StatusResponse) GetRunningServers() int64 {
+	if x != nil {
+		return x.RunningServers
+	}
+	return 0
+}
+
 var File_nexusclash_v1_game_orchestration_proto protoreflect.FileDescriptor
 
 var file_nexusclash_v1_game_orchestration_proto_rawDesc = []byte{
 	0x0a, 0x26, 0x6e, 0x65, 0x78, 0x75, 0x73, 0x63, 0x6c, 0x61, 0x73, 0x68, 0x2f, 0x76, 0x31, 0x2f,
 	0x67, 0x61, 0x6d, 0x65, 0x5f, 0x6f, 0x72, 0x63, 0x68, 0x65, 0x73, 0x74, 0x72, 0x61, 0x74, 0x69,
 	0x6f, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0d, 0x6e, 0x65, 0x78, 0x75, 0x73, 0x63,
-	0x6c, 0x61, 0x73, 0x68, 0x2e, 0x76, 0x31, 0x42, 0x4d, 0x5a, 0x4b, 0x67, 0x69, 0x74, 0x68, 0x75,
+	0x6c, 0x61, 0x73, 0x68, 0x2e, 0x76, 0x31, 0x1a, 0x1b, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2f,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x65, 0x6d, 0x70, 0x74, 0x79, 0x2e, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x22, 0x51, 0x0a, 0x0e, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x52, 0x65,
+	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12, 0x27,
+	0x0a, 0x0f, 0x72, 0x75, 0x6e, 0x6e, 0x69, 0x6e, 0x67, 0x5f, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72,
+	0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x03, 0x52, 0x0e, 0x72, 0x75, 0x6e, 0x6e, 0x69, 0x6e, 0x67,
+	0x53, 0x65, 0x72, 0x76, 0x65, 0x72, 0x73, 0x32, 0x5e, 0x0a, 0x18, 0x47, 0x61, 0x6d, 0x65, 0x4f,
+	0x72, 0x63, 0x68, 0x65, 0x73, 0x74, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x53, 0x65, 0x72, 0x76,
+	0x69, 0x63, 0x65, 0x12, 0x42, 0x0a, 0x09, 0x47, 0x65, 0x74, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73,
+	0x12, 0x16, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62,
+	0x75, 0x66, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x1a, 0x1d, 0x2e, 0x6e, 0x65, 0x78, 0x75, 0x73,
+	0x63, 0x6c, 0x61, 0x73, 0x68, 0x2e, 0x76, 0x31, 0x2e, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x52,
+	0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x42, 0x4d, 0x5a, 0x4b, 0x67, 0x69, 0x74, 0x68, 0x75,
 	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x68, 0x65, 0x69, 0x6c, 0x64, 0x6f, 0x2f, 0x6e, 0x65,
 	0x78, 0x75, 0x73, 0x2d, 0x63, 0x6c, 0x61, 0x73, 0x68, 0x2d, 0x62, 0x61, 0x63, 0x6b, 0x65, 0x6e,
 	0x64, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x6e, 0x65, 0x78, 0x75,
@@ -33,10 +103,28 @@ var file_nexusclash_v1_game_orchestration_proto_rawDesc = []byte{
 	0x6c, 0x61, 0x73, 0x68, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
-var file_nexusclash_v1_game_orchestration_proto_goTypes = []interface{}{}
+var (
+	file_nexusclash_v1_game_orchestration_proto_rawDescOnce sync.Once
+	file_nexusclash_v1_game_orchestration_proto_rawDescData = file_nexusclash_v1_game_orchestration_proto_rawDesc
+)
+
+func file_nexusclash_v1_game_orchestration_proto_rawDescGZIP() []byte {
+	file_nexusclash_v1_game_orchestration_proto_rawDescOnce.Do(func() {
+		file_nexusclash_v1_game_orchestration_proto_rawDescData = protoimpl.X.CompressGZIP(file_nexusclash_v1_game_orchestration_proto_rawDescData)
+	})
+	return file_nexusclash_v1_game_orchestration_proto_rawDescData
+}
+
+var file_nexusclash_v1_game_orchestration_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_nexusclash_v1_game_orchestration_proto_goTypes = []interface{}{
+	(*StatusResponse)(nil), // 0: nexusclash.v1.StatusResponse
+	(*emptypb.Empty)(nil),  // 1: google.protobuf.Empty
+}
 var file_nexusclash_v1_game_orchestration_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
+	1, // 0: nexusclash.v1.GameOrchestrationService.GetStatus:input_type -> google.protobuf.Empty
+	0, // 1: nexusclash.v1.GameOrchestrationService.GetStatus:output_type -> nexusclash.v1.StatusResponse
+	1, // [1:2] is the sub-list for method output_type
+	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -47,18 +135,33 @@ func file_nexusclash_v1_game_orchestration_proto_init() {
 	if File_nexusclash_v1_game_orchestration_proto != nil {
 		return
 	}
+	if !protoimpl.UnsafeEnabled {
+		file_nexusclash_v1_game_orchestration_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*StatusResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_nexusclash_v1_game_orchestration_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   0,
+			NumMessages:   1,
 			NumExtensions: 0,
-			NumServices:   0,
+			NumServices:   1,
 		},
 		GoTypes:           file_nexusclash_v1_game_orchestration_proto_goTypes,
 		DependencyIndexes: file_nexusclash_v1_game_orchestration_proto_depIdxs,
+		MessageInfos:      file_nexusclash_v1_game_orchestration_proto_msgTypes,
 	}.Build()
 	File_nexusclash_v1_game_orchestration_proto = out.File
 	file_nexusclash_v1_game_orchestration_proto_rawDesc = nil
